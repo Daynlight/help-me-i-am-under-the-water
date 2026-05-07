d079571dd4000bfa01b182544e3074cb92e02038
Daynlight <danielstodulski000@gmail.com>
2026-05-07 13:35:56 +0100

    Basic Window and CWindow lib

---
+++ UnderTheWater/main.cpp
@@ -0,0 +1,19 @@
+#include "Renderer.h"
+#include "Gui.h"
+
+
+
+
+int main(){
+  CW::Renderer::Renderer window;
+
+  while (!window.getWindowData()->should_close)
+  {
+    window.beginFrame();
+    window.swapBuffer();
+
+    window.windowEvents();
+  }
+};
+
+
